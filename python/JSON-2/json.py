f0f4c84daae3b2c3fbbd7f2c051c157547c287a4
samim kumar patel <65467139+samimcloud2020@users.noreply.github.com>
2025-07-29 13:34:19 +0530

    Update json.py

--- python/JSON-2/json.py
+++ python/JSON-2/json.py
@@ -14,3 +14,22 @@ print(python_dict)        # Output: {'name': 'Alice', 'age': 30, 'isStudent': Fa
 
 print(type(json_string))  # Output: <class 'str'>  #json string
 print(json_string)        # Output: {"name": "Alice", "age": 30, "isStudent": false, "courses": ["Math", "Science"]}
+
+
+#####################################################################################################################
+
+import json
+
+json_string = '{"name": "Bob", "age": 25, "city": "New York"}'
+
+python_dict = json.loads(json_string)
+
+print(type(json_string))  # Output: <class 'str'>
+print(json_string)        # Output: {"name": "Bob", "age": 25, "city": "New York"}
+
+print(type(python_dict))  # Output: <class 'dict'>
+print(python_dict)        # Output: {'name': 'Bob', 'age': 25, 'city': 'New York'}
+
+# You can now access elements of the dictionary:
+print(python_dict['name']) # Output: Bob
+print(python_dict['age'])  # Output: 25
